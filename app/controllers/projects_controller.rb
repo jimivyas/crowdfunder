eaaac05e91ca2b48b586745591b55582c4675022
Jimi Vyas <jimipvyas@gmail.com>
2014-12-02 15:37:31 -0500

    commented out the thing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-	before_action :require_login, except :index
+	# before_action :require_login, except :index
 
   def index
   	@projects = Project.all
