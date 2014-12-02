a666fd45aca89f8b1de5ad3dde8664750a750c7b
Jimi Vyas <jimipvyas@gmail.com>
2014-12-02 14:00:44 -0500

    created projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'projects/new'
+
+  get 'projects/create'
+
+  get 'projects/destroy'
+
+  get 'projects/editindex'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
