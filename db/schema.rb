53e9de389e45533c2ac9b0f89ab4e382daccaab0
Jimi Vyas <jimipvyas@gmail.com>
2014-12-03 15:54:48 -0500

    working on the forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203201633) do
+ActiveRecord::Schema.define(version: 20141203203840) do
 
   create_table "donates", force: true do |t|
     t.integer  "amount"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20141203201633) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
