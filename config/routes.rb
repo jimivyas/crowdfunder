9deb0a337fe7fd4630628e25bcc48e05c728d3ed
merrick read <merrick.read@gmail.com>
2014-12-03 14:14:49 -0500

    fixed resources rewards

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :user_sessions, only: [:new, :create, :destroy]
-
+  resources :rewards
+  
   get 'login' => 'user_sessions#new', as: 'login'
   get 'logout' => 'user_sessions#destroy', as: 'logout'
 
